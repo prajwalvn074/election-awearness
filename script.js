4b07407b509f84d4d678235cc58d299fcaaed2cc
prajwalvn074 <prajwalvn074@gmail.com>
2026-09-17 13:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Stay informed, check reliable information, understand the election process, and make your own informed choice.";
+}
+
+function quizAnswer() {
+    document.getElementById("quizResult").innerHTML =
+        "Answer: To choose representatives through voting.";
+}
